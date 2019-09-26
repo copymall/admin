3bfe848292ce474ee43e1e2e7cde52a589322d2c
li8607 <maverick_8090@163.com>
2019-09-26 21:59:51 +0800

    添加商品

--- src/views/pms/productAttr/index.vue
+++ src/views/pms/productAttr/index.vue
@@ -12,22 +12,28 @@
                       v-loading="listLoading"
                       border>
                 <el-table-column label="编号" width="100" align="center">
-                    <template slot-scope="scope"></template>
+                    <template slot-scope="scope">{{scope.row.id}}</template>
                 </el-table-column>
                 <el-table-column label="类型名称" align="center">
-                    <template slot-scope="scope"></template>
+                    <template slot-scope="scope">{{scope.row.name}}</template>
                 </el-table-column>
                 <el-table-column label="属性数量" width="200" align="center">
-                    <template slot-scope="scope"></template>
+                    <template slot-scope="scope">{{scope.row.attributeCount==null?0:scope.row.attributeCount}}</template>
                 </el-table-column>
                 <el-table-column label="参数数量" width="200" align="center">
-                    <template slot-scope="scope"></template>
+                    <template slot-scope="scope">{{scope.row.paramCount==null?0:scope.row.paramCount}}</template>
                 </el-table-column>
                 <el-table-column label="设置" width="200" align="center">
-                    <template slot-scope="scope"></template>
+                    <template slot-scope="scope">
+                        <el-button size="mini" @click="getAttrList(scope.$index, scope.row)">属性列表</el-button>
+                        <el-button size="mini" @click="getParamList(scope.$index, scope.row)">参数列表</el-button>
+                    </template>
                 </el-table-column>
                 <el-table-column label="操作" width="200" align="center">
-                    <template slot-scope="scope"></template>
+                    <template slot-scope="scope">
+                        <el-button size="mini" @click="handleUpdate(scope.$index, scope.row)">编辑</el-button>
+                        <el-button size="mini" type="danger" @click="handleUpdate(scope.$index, scope.row)">删除</el-button>
+                    </template>
                 </el-table-column>
             </el-table>
         </div>
@@ -35,19 +41,42 @@
             <el-pagination background
                            :total="total"
                            :page-sizes="[5, 10, 15]"
+                           :page-size="listQuery.pageSize"
+                           :current-page.sync="listQuery.pageNum"
+                           @size-change="handleSizeChange"
+                           @current-change="handleCurrentChange"
                            layout="total, sizes,prev, pager, next,jumper"></el-pagination>
         </div>
     </div>
 </template>
 
 <script>
+    import {fetchList} from "../../../api/productAttrCate";
+
     export default {
         name: "productAttr",
         methods:{
-            addProductAttrCate(){},
+            addProductAttrCate(){
+                // this.$router.push({path:''})
+            },
             getList() {
                 this.listLoading = true;
-                fetchList(listQuery).then(response=>{})
+                fetchList(this.listQuery).then(response=>{
+                    this.listLoading = false;
+                    this.list = response.data.list;
+                    this.total = response.data.total;
+                });
+            },
+            getAttrList(){},
+            getParamList(){},
+            handleSizeChange(val){
+                this.listQuery.pageSize = val;
+                this.listQuery.pageNum = 0;
+                this.getList();
+            },
+            handleCurrentChange(val){
+                this.listQuery.pageNum = val;
+                this.getList();
             }
         },
         data() {
